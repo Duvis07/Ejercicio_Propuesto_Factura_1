244270876c3506a338972c09b3a199a162d0475e
duvan.botero <duvis07>
2023-04-10 14:56:01 -0500

    Se relaÃaliz mejora del ejercicio de la facturaa

--- src/main/scala/com/co/sura/classes/ProductThree.scala
+++ src/main/scala/com/co/sura/classes/ProductThree.scala
@@ -3,7 +3,7 @@ package com.co.sura.classes
 import com.co.sura.traits.CalcularProducto
 import com.co.sura.typeAlias.TypeAlias.{Cantidad, Nombre, Precio}
 
-class ProductThree(nombre:Nombre , precio:Precio , cantidad:Cantidad ) extends CalcularProducto {
+class ProductThree(nombre: Nombre, precio: Precio, cantidad: Cantidad) extends CalcularProducto {
 
   def valorBruto(): Precio = {
     val resultado = precio * cantidad
@@ -11,17 +11,17 @@ class ProductThree(nombre:Nombre , precio:Precio , cantidad:Cantidad ) extends C
   }
 
 
-  override def valorDelIva(iva: Precio=19D): Precio ={
+  override def valorDelIva(iva: Precio = 19D): Precio = {
     val resultado = precio * (iva / 100)
     resultado
   }
 
-  override def valorDeImpuesto(impuesto: Precio=18D): Precio ={
+  override def valorDeImpuesto(impuesto: Precio = 18D): Precio = {
     val resultado = precio * (impuesto / 100)
     resultado
   }
 
-  override def totalGeneral(iva: Precio=19D, impuesto: Precio=16D): Unit = {
+  override def totalGeneral(iva: Precio = 19D, impuesto: Precio = 16D): Unit = {
     val valorBruto = this.valorBruto()
     val valorDelIva = this.valorDelIva(iva)
     val valorDeImpuestos = this.valorDeImpuesto(impuesto)
